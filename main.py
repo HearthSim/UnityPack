1204423d6c2207656438cc2da3fe2036d59a59c4
Jerome Leclanche <jerome@leclan.ch>
2016-01-03 06:05:46 +0200

    Read TypeMetadata and TypeTree

--- main.py
+++ main.py
@@ -9,8 +9,9 @@ SIGNATURE_RAW = "UnityRaw"
 
 
 class BinaryReader:
-	def __init__(self, buf):
+	def __init__(self, buf, endian="<"):
 		self.buf = buf
+		self.endian = endian
 
 	def read_string(self, encoding="utf-8"):
 		ret = []
@@ -31,14 +32,59 @@ class BinaryReader:
 	def tell(self):
 		return self.buf.tell()
 
+	def read_byte(self):
+		return struct.unpack(self.endian + "b", self.read(1))[0]
+
 	def read_int(self):
-		return struct.unpack(">i", self.read(4))[0]
+		return struct.unpack(self.endian + "i", self.read(4))[0]
 
 	def read_uint(self):
-		return struct.unpack(">I", self.read(4))[0]
+		return struct.unpack(self.endian + "I", self.read(4))[0]
+
+
+class TypeTree:
+	def load_blob(self, buf):
+		self.nodes = buf.read_uint()
+		self.buffer_bytes = buf.read_uint()
+		node_data = BytesIO(buf.read(24 * self.nodes))
+		local_buffer = BytesIO(buf.read(self.buffer_bytes))
+		# todo the rest...
+
+
+class TypeMetadata:
+	def load(self, buf):
+		offset = buf.tell()
+		self.generator_version = buf.read_string()
+		self.target_platform = buf.read_uint()
+
+		assert self.target_platform == 5  # Windows. RuntimePlatform?
+
+		# if format >= 13
+		self.has_type_trees = bool(buf.read_byte())
+		self.num_types = buf.read_int()
+
+		hashes = {}
+		trees = {}
+
+		for i in range(self.num_types):
+			class_id = buf.read_int()  # TODO get unity class
+			if class_id < 0:
+				hash = buf.read(0x20)
+			else:
+				hash = buf.read(0x10)
+
+			hashes[class_id] = hash
+
+			if self.has_type_trees:
+				tree = TypeTree()
+				tree.load_blob(buf)
+				trees[class_id] = tree
 
 
 class Asset:
+	def __init__(self):
+		self.objects = {}
+
 	def __repr__(self):
 		return "<Asset %s>" % (self.name)
 
@@ -54,9 +100,15 @@ class Asset:
 		self.data_offset = buf.read_uint()
 		self.endianness = buf.read_uint()
 
-		assert self.endianness == 0
+		if self.endianness == 0:
+			buf.endian = "<"
+
 		assert self.format >= 9
 
+		self.tree = TypeMetadata()
+		self.tree.load(buf)
+
+		buf.seek(offset + self.data_offset)
 		self.data = BytesIO(buf.read(self.size))
 
 
@@ -75,17 +127,13 @@ class AssetBundle:
 		if self.file:
 			self.file.close()
 
-	@property
-	def files(self):
-		return []
-
 	def load_file(self, path):
 		file = open(path, "rb")
 		self.file = file
 		self.read_header()
 
 	def read_header(self):
-		buf = BinaryReader(self.file)
+		buf = BinaryReader(self.file, endian=">")
 		self.signature = buf.read_string()
 		self.format_version = buf.read_int()
 		self.unity_version = buf.read_string()
@@ -121,7 +169,11 @@ def main():
 	files = sys.argv[1:]
 	for file in files:
 		bundle = AssetBundle.from_path(file)
-		print(bundle.files)
+		print(bundle)
+		for asset in bundle.assets:
+			print(asset)
+			for obj in asset.objects:
+				print(obj)
 
 
 if __name__ == "__main__":
