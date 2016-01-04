a44c30486246df4aafc0a41adf122b5697e5cdd6
Jerome Leclanche <jerome@leclan.ch>
2016-01-04 08:26:17 +0200

    Implement DXT5 decoding

--- unitypack/dds.py
+++ unitypack/dds.py
@@ -95,7 +95,7 @@ def _c2b(a, b):
 	return (a + b) // 2
 
 def _c3(a, b):
-	return (2 * a + b) // 3
+	return (2 * b + a) // 3
 
 
 def dxt1(data, width, height):
@@ -132,5 +132,61 @@ def dxt1(data, width, height):
 
 					idx = 4 * ((y + j) * width + (x + i))
 					ret[idx:idx+4] = bytes([r, g, b, 0xff])
+
+	return ret
+
+
+def _dxtc_alpha(a0, a1, ac0, ac1, ai):
+	if ai <= 12:
+		ac = (ac0 >> ai) & 7
+	elif ai == 15:
+		ac = (ac0 >> 15) | ((ac1 << 1) & 6)
+	else:
+		ac = (ac1 >> (ai - 16)) & 7
+
+	if ac == 0:
+		alpha = a0
+	elif ac == 1:
+		alpha = a1
+	elif a0 > a1:
+		alpha = ((8 - ac) * a0 + (ac - 1) * a1) // 7
+	elif ac == 6:
+		alpha = 0
+	elif ac == 7:
+		alpha = 0xff
+	else:
+		alpha = ((6 - ac) * a0 + (ac - 1) * a1) // 5
+
+	return alpha
+
+
+def dxt5(data, width, height):
+	ret = bytearray(4 * width * height)
+	data = BytesIO(data)
+
+	for y in range(0, height, 4):
+		for x in range(0, width, 4):
+			a0, a1, ac0, ac1, c0, c1, code = struct.unpack("<2BHI2HI", data.read(16))
+
+			r0, g0, b0 = decode565(c0)
+			r1, g1, b1 = decode565(c1)
+
+			for j in range(4):
+				for i in range(4):
+					ai = 3 * (4 * j + i)
+					alpha = _dxtc_alpha(a0, a1, ac0, ac1, ai)
+
+					cc = (code >> 2 * (4 * j + i)) & 3
+					if cc == 0:
+						r, g, b = r0, g0, b0
+					elif cc == 1:
+						r, g, b = r1, g1, b1
+					elif cc == 2:
+						r, g, b = _c2a(r0, r1), _c2a(g0, g1), _c2a(b0, b1)
+					elif cc == 3:
+						r, g, b = _c3(r0, r1), _c3(g0, g1), _c3(b0, b1)
+
+					idx = 4 * ((y + j) * width + (x + i))
+					ret[idx:idx+4] = bytes([r, g, b, alpha])
 
 	return ret
