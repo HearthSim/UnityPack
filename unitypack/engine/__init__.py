37ae87cefa1507f0a4ad70f5cd74f871ffafd60d
Jerome Leclanche <jerome@leclan.ch>
2016-01-27 10:11:58 +0200

    Implement {Mesh,Ellipsoid,}ParticleEmitter classes

--- unitypack/engine/__init__.py
+++ unitypack/engine/__init__.py
@@ -6,7 +6,7 @@ from .audio import AudioClip, AudioSource, StreamedResource
 from .component import Behaviour, Component, Transform
 from .mesh import Mesh, MeshFilter
 from .object import GameObject
-from .particle import EllipsoidParticleEmitter, ParticleEmitter, ParticleSystem
+from .particle import EllipsoidParticleEmitter, MeshParticleEmitter, ParticleEmitter, ParticleSystem
 from .physics import BoxCollider, BoxCollider2D, Collider, Collider2D, Rigidbody2D
 from .renderer import MeshRenderer, ParticleRenderer, ParticleSystemRenderer, Renderer
 from .text import TextAsset, TextMesh, Shader
