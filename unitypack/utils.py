5ddd33e9ffa2248b08004e41749412f0bc405f53
Jacqueline <jack980517@users.noreply.github.com>
2017-01-18 23:54:17 +0800

    Update utils.py
    Uses a pure python implementation of lz4 decompression to avoid needs of native code compilation.
    Code borrowed from https://gist.github.com/weigon/43e217e69418875a55b31b1a5c89662d

--- unitypack/utils.py
+++ unitypack/utils.py
@@ -1,16 +1,86 @@
 import struct
 from os import SEEK_CUR
+from io import BytesIO
+from six.moves import xrange  # pylint: disable=redefined-builtin
+from six import byte2int
+
+def uncompress(src): #from https://gist.github.com/weigon/43e217e69418875a55b31b1a5c89662d
+	"""uncompress a block of lz4 data.
+
+	:param bytes src: lz4 compressed data (LZ4 Blocks)
+	:returns: uncompressed data
+	:rtype: bytearray
+
+	.. seealso:: http://cyan4973.github.io/lz4/lz4_Block_format.html
+	"""
+	src = BytesIO(src)
+
+	# if we have the original size, we could pre-allocate the buffer with
+	# bytearray(original_size), but then we would have to use indexing
+	# instad of .append() and .extend()
+	dst = bytearray()
+	min_match_len = 4
+
+	def get_length(src, length):
+		"""get the length of a lz4 variable length integer."""
+		if length != 0x0f:
+			return length
+
+		while True:
+			read_buf = src.read(1)
+			if len(read_buf) != 1:
+				raise Exception("EOF at length read")
+			len_part = byte2int(read_buf)
+
+			length += len_part
+
+			if len_part != 0xff:
+				break
+
+		return length
+
+	while True:
+		# decode a block
+		read_buf = src.read(1)
+		if len(read_buf) == 0:
+			raise Exception("EOF at reading literal-len")
+		token = byte2int(read_buf)
+
+		literal_len = get_length(src, (token >> 4) & 0x0f)
+
+		# copy the literal to the output buffer
+		read_buf = src.read(literal_len)
+
+		if len(read_buf) != literal_len:
+			raise Exception("not literal data")
+		dst.extend(read_buf)
+
+		read_buf = src.read(2)
+		if len(read_buf) == 0:
+			if token & 0x0f != 0:
+				raise Exception("EOF, but match-len > 0: %u" % (token % 0x0f, ))
+			break
+
+		if len(read_buf) != 2:
+			raise Exception("premature EOF")
+
+		offset = byte2int([read_buf[0]]) | (byte2int([read_buf[1]]) << 8)
+
+		if offset == 0:
+			raise Exception("offset can't be 0")
+
+		match_len = get_length(src, (token >> 0) & 0x0f)
+		match_len += min_match_len
+
+		# append the sliding window of the previous literals
+		for _ in xrange(match_len):
+			dst.append(dst[-offset])
+
+	return dst
 
 
 def lz4_decompress(data, size):
-	try:
-		import lz4
-	except ImportError:
-		raise RuntimeError("python-lz4 is required to read UnityFS files")
-
-	# https://github.com/python-lz4/python-lz4/issues/6
-	data = struct.pack("i", size) + data
-	return lz4.loads(data)
+	return uncompress(data)
 
 
 def extract_audioclip_samples(d) -> dict:
