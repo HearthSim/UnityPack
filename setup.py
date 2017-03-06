32dd5feb06f578f4ccf0eafe118b7d74d8c47e52
Jerome Leclanche <jerome@leclan.ch>
2017-03-06 11:33:28 +0200

    Require the optional dependencies on install_requires

--- setup.py
+++ setup.py
@@ -10,4 +10,10 @@ setup(
 	packages=find_packages(),
 	package_data={"": ["classes.json", "strings.dat", "structs.dat"]},
 	scripts=["bin/unityextract", "bin/unity2yaml"],
+	install_requires=[
+		"decrunch",
+		"fsb5",
+		"lz4",
+		"Pillow",
+	],
 )
