003e3e015518b062633788c3e53ecb7c7e591908
Jerome Leclanche <jerome@leclan.ch>
2017-03-10 11:33:27 +0200

    Move setup options to setup.cfg

--- setup.py
+++ setup.py
@@ -1,17 +1,6 @@
 #!/usr/bin/env python3
 
-from setuptools import setup, find_packages
+from setuptools import setup
 
 
-setup(
-	name="unitypack",
-	packages=find_packages(),
-	package_data={"": ["classes.json", "strings.dat", "structs.dat"]},
-	scripts=["bin/unityextract", "bin/unity2yaml"],
-	install_requires=[
-		"decrunch",
-		"fsb5",
-		"lz4",
-		"Pillow",
-	],
-)
+setup()
