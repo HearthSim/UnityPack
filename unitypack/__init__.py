5e886e6a9e5a7088d93ad66bf63a3dee3a705fc2
Richard Gale <richard@richardgale.org>
2017-05-25 20:45:11 -0700

    Exposes get_asset_by_filename as load_from_file

--- unitypack/__init__.py
+++ unitypack/__init__.py
@@ -10,3 +10,10 @@ def load(file, env=None):
 	if env is None:
 		env = UnityEnvironment()
 	return env.load(file)
+
+def load_from_file(file, env=None):
+    from .environment import UnityEnvironment
+
+    if env is None:
+        env = UnityEnvironment()
+    return env.get_asset_by_filename(file)
