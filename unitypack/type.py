32b8b096a2ff7e40d8bcefcf6f18cfc04ac301cf
Steven Vergenz <steven.vergenz@microsoft.com>
2019-12-19 17:35:49 -0800

    Add format check to new node size

--- unitypack/type.py
+++ unitypack/type.py
@@ -52,7 +52,8 @@ class TypeTree:
 	def load_blob(self, buf):
 		num_nodes = buf.read_uint()
 		self.buffer_bytes = buf.read_uint()
-		node_data = BytesIO(buf.read(32 * num_nodes))
+		node_bytes = 32 if self.format >= 20 else 24
+		node_data = BytesIO(buf.read(node_bytes * num_nodes))
 		self.data = buf.read(self.buffer_bytes)
 
 		parents = [self]
@@ -79,7 +80,9 @@ class TypeTree:
 			curr.size = buf.read_int()
 			curr.index = buf.read_uint()
 			curr.flags = buf.read_int()
-			unk1 = buf.read_int64() # zero bytes?
+
+			if self.format >= 20:
+				unk1 = buf.read_int64() # zero bytes?
 
 	def get_string(self, offset):
 		if offset < 0:
